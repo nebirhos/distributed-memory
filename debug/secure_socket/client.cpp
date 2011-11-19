268b6e1c313d6bb1ed13fbef8f0c1c17fe1add44
nebirhos <nebirhos@aol.com>
2011-11-19 04:24:58 +0100

    debug tool updates

--- debug/secure_socket/client.cpp
+++ debug/secure_socket/client.cpp
@@ -1,5 +1,6 @@
 #include <dm/socket/socket_client.h>
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
 
@@ -9,9 +10,14 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  DM::SocketClient s( argv[1], argv[2], argv[3], argv[4] );
-
-  s << "The narwhal bacons at midnight";
+  try {
+    DM::SocketClient s( argv[1], argv[2], argv[3], argv[4] );
+    s << "The narwhal bacons at midnight";
+  }
+  catch (const runtime_error& e) {
+    cout << "runtime_error: " << e.what() << endl;
+    return 1;
+  }
 
   return 0;
 }
--- debug/secure_socket/server.cpp
+++ debug/secure_socket/server.cpp
@@ -10,23 +10,33 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  DM::SocketServer listen( argv[1], argv[2], argv[3] );
+  try {
+    DM::SocketServer listen( argv[1], argv[2], argv[3] );
 
-  while (1) {
-    DM::SocketServer* incoming = new DM::SocketServer();
-    try {
-      listen.accept( *incoming );
-    }
-    catch (runtime_error e) {
+    while (1) {
+      DM::SocketServer* incoming = new DM::SocketServer();
+
+      try {
+        listen.accept( *incoming );
+
+        string message;
+        (*incoming) >> message;
+        cout << "Message: ---" << message << "---" << endl;
+      }
+      catch (runtime_error e) {
+        cout << "runtime_error: " << e.what() << endl;
+        delete incoming;
+        continue;
+      }
+
       delete incoming;
-      continue;
     }
-
-    string message;
-    (*incoming) >> message;
-    cout << "Message: ---" << message << "---" << endl;
-    delete incoming;
   }
+  catch (const runtime_error& e) {
+    cout << "runtim_error: " << e.what() << endl;
+    return 1;
+  }
+
 
   return 0;
 }
