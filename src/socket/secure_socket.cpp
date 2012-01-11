671d78de8ad78c450b5ca74575ac3b79a72c623b
nebirhos <nebirhos@aol.com>
2012-01-11 01:53:15 +0100

    fixed a security bug in key exchange protocol

--- src/socket/secure_socket.cpp
+++ src/socket/secure_socket.cpp
@@ -36,8 +36,8 @@ bool SecureSocket::open_server( const string port, const string privkey, const s
     return false;
   }
   // Pass must fit in a single RSA packet!
-  if ( (int)pass.size() > m_rsa_size - 2*EVP_MAX_KEY_LENGTH ) {
-    Logger::error() << "passphrase too long! Max allowed length is " << m_rsa_size - 2*EVP_MAX_KEY_LENGTH << " characters " << endl;
+  if ( (int)pass.size() > m_rsa_size - 3*EVP_MAX_KEY_LENGTH ) {
+    Logger::error() << "passphrase too long! Max allowed length is " << m_rsa_size - 3*EVP_MAX_KEY_LENGTH << " characters " << endl;
     return false;
   }
   else {
@@ -59,47 +59,56 @@ bool SecureSocket::open_client( const string host, const string port, const stri
 
   // SESSION KEY GENERATION
   int rounds = 5;
-  unsigned char nonce[ EVP_MAX_KEY_LENGTH ], salt[8];
-  RAND_bytes( nonce, EVP_MAX_KEY_LENGTH );
+  unsigned char nonce_a[ EVP_MAX_KEY_LENGTH ], salt[8];
+  RAND_bytes( nonce_a, EVP_MAX_KEY_LENGTH );
   RAND_bytes( salt, 8 );
 
   int res = EVP_BytesToKey( CIPHER_ALG, EVP_sha1(), salt,
                             (unsigned char*) m_pass.data(),
-                            m_pass.size(), rounds, m_session_key, nonce );
+                            m_pass.size(), rounds, m_session_key, nonce_a );
   if (res <= 0) {
     Logger::debug() << "cannot generate session key" << endl;
     return false;
   }
 
-  // M1: Request SEND
-  unsigned char message[ m_rsa_size ], encrypted[ m_rsa_size ];
-  int message_size = 2*EVP_MAX_KEY_LENGTH + m_pass.size();
+  // M1 RECEIVE
+  unsigned char nonce_b[ EVP_MAX_KEY_LENGTH ];
+  int nonce_size = recv( (char*) nonce_b, EVP_MAX_KEY_LENGTH );
+  if ( nonce_size != EVP_MAX_KEY_LENGTH ) {
+    Logger::error() << "cannot receive M1" << endl;
+    return false;
+  }
 
-  memcpy( message, nonce, EVP_MAX_KEY_LENGTH );
-  memcpy( message+EVP_MAX_KEY_LENGTH, m_session_key, EVP_MAX_KEY_LENGTH );
-  memcpy( message+2*EVP_MAX_KEY_LENGTH, m_pass.data(), m_pass.size() );
+  // M2 SEND
+  unsigned char message[ m_rsa_size ], encrypted[ m_rsa_size ];
+  int message_size = 3*EVP_MAX_KEY_LENGTH + m_pass.size();
+
+  memcpy( message+0*EVP_MAX_KEY_LENGTH, m_session_key, EVP_MAX_KEY_LENGTH );
+  memcpy( message+1*EVP_MAX_KEY_LENGTH, nonce_a, EVP_MAX_KEY_LENGTH );
+  memcpy( message+2*EVP_MAX_KEY_LENGTH, nonce_b, EVP_MAX_KEY_LENGTH );
+  memcpy( message+3*EVP_MAX_KEY_LENGTH, m_pass.data(), m_pass.size() );
 
   int encrypted_size = RSA_public_encrypt( message_size, message, encrypted, m_rsa_pubkey->pkey.rsa, RSA_PKCS1_PADDING );
   if ( encrypted_size != m_rsa_size ) {
-    Logger::error() << "cannot encrypt M1" << endl;
+    Logger::error() << "cannot encrypt M2" << endl;
     return false;
   }
   if ( ! send( (char*) encrypted, m_rsa_size ) ) {
-    Logger::debug() << "cannot send M1" << endl;
+    Logger::debug() << "cannot send M2" << endl;
     return false;
   }
 
-  // M2: Response RECEIVE
+  // M3 RECEIVE
   unsigned char* decrypted;
   int decrypted_size = recv_secure( decrypted );
   if ( decrypted_size <= 0 ) {
-    Logger::debug() << "cannot decrypt M2" << endl;
+    Logger::debug() << "cannot decrypt M3" << endl;
     return false;
   }
   if ( (decrypted_size != 2*EVP_MAX_KEY_LENGTH) ||
-       memcmp( decrypted, nonce, EVP_MAX_KEY_LENGTH ) ||
-       memcmp( decrypted+EVP_MAX_KEY_LENGTH, m_session_key, EVP_MAX_KEY_LENGTH ) ) {
-    Logger::debug() << "wrong M2 received" << endl;
+       memcmp( decrypted, m_session_key, EVP_MAX_KEY_LENGTH ) ||
+       memcmp( decrypted+EVP_MAX_KEY_LENGTH, nonce_a, EVP_MAX_KEY_LENGTH ) ) {
+    Logger::debug() << "wrong M3 received" << endl;
     delete[] decrypted;
     return false;
   }
@@ -112,26 +121,35 @@ bool SecureSocket::open_client( const string host, const string port, const stri
 bool SecureSocket::accept( SecureSocket& socket ) const {
   if ( ! Socket::accept(socket) )  return false;
 
-  // M1: Request RECEIVE
+  // M1 SEND
+  unsigned char nonce_b[ EVP_MAX_KEY_LENGTH ];
+  RAND_bytes( nonce_b, EVP_MAX_KEY_LENGTH );
+  if ( ! socket.send( (char*) nonce_b, EVP_MAX_KEY_LENGTH ) ) {
+    Logger::error() << "cannot send M1" << endl;
+    return false;
+  }
+
+  // M2 RECEIVE
   unsigned char message[ m_rsa_size ], decrypted[ m_rsa_size ];
-  int message_size = 2*EVP_MAX_KEY_LENGTH + m_pass.size();
+  int message_size = 3*EVP_MAX_KEY_LENGTH + m_pass.size();
 
   int encrypted_size = socket.recv( (char*) message, m_rsa_size );
   if ( encrypted_size != m_rsa_size ) {
-    Logger::error() << "cannot receive M1" << endl;
+    Logger::error() << "cannot receive M2" << endl;
     return false;
   }
   int decrypted_size = RSA_private_decrypt( m_rsa_size, message, decrypted, m_rsa_privkey->pkey.rsa, RSA_PKCS1_PADDING );
   if ( (decrypted_size != message_size) ||
-       memcmp( m_pass.data(), decrypted + 2*EVP_MAX_KEY_LENGTH, m_pass.size() ) ) {
-    Logger::error() << "error on M1 decryption or wrong passphrase" << endl;
+       memcmp( decrypted+2*EVP_MAX_KEY_LENGTH, nonce_b, EVP_MAX_KEY_LENGTH ) ||
+       memcmp( decrypted+3*EVP_MAX_KEY_LENGTH, m_pass.data(), m_pass.size() ) ) {
+    Logger::error() << "error on M2 decryption or wrong passphrase" << endl;
     return false;
   }
-  memcpy( socket.m_session_key, decrypted+EVP_MAX_KEY_LENGTH, EVP_MAX_KEY_LENGTH );
+  memcpy( socket.m_session_key, decrypted, EVP_MAX_KEY_LENGTH );
 
-  // M2: Response SEND
+  // M3 SEND
   if ( ! socket.send_secure( decrypted, 2*EVP_MAX_KEY_LENGTH ) ) {
-    Logger::error() << "cannot send M2" << endl;
+    Logger::error() << "cannot send M3" << endl;
     return false;
   }
 
